70a81222ca6816d9b74e7c7eeeaab6d29eb9d0aa
Kacper Wojciechowski <k.woj.coding@gmail.com>
2024-05-01 21:36:07 +0000

    Implemented Solar systems generation

--- src/GameMap/Galaxy.cpp
+++ src/GameMap/Galaxy.cpp
@@ -1,4 +1,5 @@
 #include <GameMap/Galaxy.h>
+#include <GameMap/SolarSystem.h>
 #include <Utility/RandomGenerator.h>
 
 namespace GameMap
@@ -11,10 +12,30 @@ std::size_t randomizeLayersCount()
 	constexpr auto upperBound = 10;
 	return static_cast<std::size_t>(utility::RandomGenerator::getInt(lowerBound, upperBound));
 }
+std::size_t randomizeSolarSystemsCount()
+{
+	constexpr auto lowerBound = 1;
+	constexpr auto upperBound = 4;
+	return static_cast<std::size_t>(utility::RandomGenerator::getInt(lowerBound, upperBound));
+}
 } // namespace::
 Galaxy::Galaxy(Time::GameTimeService& gameTimeService)
 {
 	auto layersCount = randomizeLayersCount();
 	layers.resize(layersCount);
+
+	for (auto layer : layers)
+	{
+		auto solarSystemsCount = randomizeSolarSystemsCount();
+		for (std::size_t i = 0; i < solarSystemsCount; i++)
+		{
+			layer.emplace_back(std::move(SolarSystem::create(gameTimeService)));
+		}
+		interconnectSolarSystems();
+	}
+}
+
+void Galaxy::interconnectSolarSystems()
+{
 }
 } // namespace GameMap
